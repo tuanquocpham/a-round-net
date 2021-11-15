c93b2b5e6409ec93a301bd7ebedb94e45d733c42
tuan <tuan0pham@gmail.com>
2021-11-15 11:00:49 -0500

    Initial design

---
+++ script.js
@@ -0,0 +1,28 @@
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+  /* Toggle between adding and removing the "active" class,
+  to highlight the button that controls the panel */
+  this.classList.toggle("active");
+
+  /* Toggle between hiding and showing the active panel */
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "block";
+  }
+});
+}
+
+
+// Scrolling title
+(function titleScroller(text) {
+  document.title = text;
+  setTimeout(function () {
+      titleScroller(text.substr(1) + text.substr(0, 1));
+  }, 500);
+}(" Typography & Interaction 2: Section B, Spring 2022 "));

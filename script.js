b5961be1abe0066e6b15eaf93c30e4989c1e8911
tuan <tuan0pham@gmail.com>
2022-01-29 18:29:06 -0500

    Update

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ acc[i].addEventListener("click", function() {
       titleScroller(text.substr(1) + text.substr(0, 1));
   }, 500);
 }(" Typography & Interaction 2: Section B, Spring 2022 "));
+
+
+// randomizer
+const array = ["Krishangi", "Liam", "Jess", "Antoinette", "Madhu", "Amy", "Bryan", "So Young", "Giordana", "Maria", "Carla", "Thao", "Ashley"]
+
+const arrayClone = array.slice()
+let arrayLength = array.length
+let newArray = []
+
+for (  let i = 0; i < 13; i++) {
+    let arr = arrayClone[Math.floor(Math.random() * arrayLength)]
+    arrayLength--
+    let index = arrayClone.indexOf(arr)
+    arrayClone.splice(index, 1)
+    newArray.push(arr)
+ }
+
+  randomizer.onclick = function() {
+    console.log(newArray);
+}
